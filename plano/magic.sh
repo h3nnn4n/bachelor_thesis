b0e97feaf9c647c651d2549d513f5ed9270d46c1
h3nnn4n <uber.renan@gmail.com>
2016-08-10 12:01:05 -0300

    Now using a separate file to draw the graphs

--- plano/magic.sh
+++ plano/magic.sh
@@ -4,6 +4,12 @@ COMP=pdflatex
 BIB_COMP=bibtex
 MAIN=tccplanoudesc
 MAIN_BIB=tccplanoudesc
+OTHERS=graph
+
+for i in $OTHERS
+do
+    ${COMP} $i
+done
 
 ${COMP}       ${MAIN}
 ${BIB_COMP}   ${MAIN}
